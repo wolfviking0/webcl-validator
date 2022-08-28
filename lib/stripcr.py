bce13cea16ef20a2dd1006170656f19dddf874f7
Anthony Liot <wolfviking0@yahoo.com>
2022-08-28 12:06:28 -0700

    Update permission
    Signed-off-by: Anthony Liot <wolfviking0@yahoo.com>

--- lib/stripcr.py
+++ lib/stripcr.py
@@ -1,4 +1,4 @@
 # git on windows might mess up line endings 
 # added separate script because sed on osx does not work for removing CRs
 import sys
-open(sys.argv[2], "wb+").write(open(sys.argv[1],"rb").read().replace('\r',''))
+open(sys.argv[2], "wb+").write(open(sys.argv[1],"rb").read().decode().replace('\r','').encode())
